36c15e02d2bc5dd9f5770cd7055b724fc944e44a
Bryan Maina <sourcedeep1@gmail.com>
2017-01-19 17:57:39 +0100

    multi-lines editing

---
+++ .emacs.d/config/editing/setup-multilinesedit.el
@@ -0,0 +1,12 @@
+(use-package multiple-cursors
+  :config
+  ;; multiple-cursors
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-+") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+  ;; From active region to multiple cursors:
+  (global-set-key (kbd "C-c q r") 'set-rectangular-region-anchor)
+  (global-set-key (kbd "C-c q c") 'mc/edit-lines)
+  (global-set-key (kbd "C-c q e") 'mc/edit-ends-of-lines)
+  (global-set-key (kbd "C-c q a") 'mc/edit-beginnings-of-lines))
