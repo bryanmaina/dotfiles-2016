6f7c294b30ed4a6820d4b8993119fe05890db13e
Bryan Maina <sourcedeep1@gmail.com>
2017-02-20 15:24:00 +0100

    Makes emacs theme play well with terminal theme

--- .emacs.d/config/interface/setup-scheme.el
+++ .emacs.d/config/interface/setup-scheme.el
@@ -3,4 +3,11 @@
   :ensure t
   :config
   (color-theme-initialize)
-  (color-theme-hober))
+;;  (color-theme-hober))
+  (color-theme-andreas))
+
+(defun on-frame-open (frame)
+  (if (not (display-graphic-p frame))
+      (set-face-background 'default "unspecified-bg" frame)))
+(on-frame-open (selected-frame))
+(add-hook 'after-make-frame-functions 'on-frame-open)
